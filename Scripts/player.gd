762aaf177b60d125345d0f56d2fed3868597b301
Mark Nair <mark@TRM-TBD.AscensionAcademy.local>
2025-10-30 09:54:12 -0500

    Refactored to split into function calls

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,24 +12,35 @@ var move_input : float
 
 func _physics_process(delta: float) -> void:
 	
-	# Gravity
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	apply_gravity(delta)
 	
-	# Movement Input
-	move_input = Input.get_axis("move_left","move_right")
+	get_movement_input()
 	
-	# Movement
-	if move_input != 0:
-		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
-	else:
-		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+	apply_movement(delta)
 	
-	# Add your jumping code here.
-	
+	jump()
 	
 	move_and_slide()
 	
 func _process(delta: float) -> void:
 	if velocity.x != 0:
 		sprite.flip_h = velocity.x > 0
+		
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+func get_movement_input():
+	move_input = Input.get_axis("move_left","move_right")
+	
+func apply_movement(delta):
+	if move_input != 0:
+		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+		
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+
+	
